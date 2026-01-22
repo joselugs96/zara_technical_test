a13f9961b557edd155a3fb5eb889ede7272f69e1
Jose Luis <joselugs96@gmail.com>
2026-01-22 21:30:21 +0100

    chore: add Jest and React Testing Library with test structure

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(config);
